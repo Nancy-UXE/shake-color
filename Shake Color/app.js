e9d47ec1966808d1b14ba24752a5f539d5b85c5e
Nancy Siam <111718786+Nancy-UXE@users.noreply.github.com>
2023-01-03 21:49:26 -0800

    Add copy functionality - copy button
    Adding the copy button JS code, in order to copy the value of the color after shaking it

--- Shake Color/app.js
+++ Shake Color/app.js
@@ -3,6 +3,7 @@ const btn = document.getElementById("btn");
 const color = document.querySelector(".color");
 const textColor = document.getElementsByTagName("h1")[0];
 let clearClass;
+let copyText = document.querySelector(".copy-text");
 
 btn.addEventListener("click", function () {
   if (clearClass) {
@@ -20,6 +21,24 @@ btn.addEventListener("click", function () {
   clearClass = setTimeout(() => {
     textColor.classList.remove("shake-me");
   }, "500");
+});
+
+copyText.querySelector("button").addEventListener("click", function () {
+  /* Because of some security restrictions you can only execute the Copy command 
+  if the user interacted with the page, so you have to add a button and copy 
+  the text after the user clicks on the button. */
+  let textArea = document.createElement("textarea");
+
+  textArea.value = color.textContent;
+  document.body.appendChild(textArea);
+  textArea.select();
+  document.execCommand("copy");
+  copyText.classList.add("active");
+  window.getSelection().removeAllRanges();
+  setTimeout(function () {
+    copyText.classList.remove("active");
+  }, 1000);
+  textArea.remove();
 });
 
 function getRandomNumber() {
